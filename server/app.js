c389e7e9da52ad03722441e6cdbb0bd58a50b194
cmmisura14 <cmmisura14@gmail.com>
2015-05-07 16:55:34 -0500

    Test

--- server/app.js
+++ server/app.js
@@ -6,9 +6,10 @@ var path = require('path');
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/*', function(request, response){
-    var file = request.params[0] || 'views/index.html';
-
-    response.sendFile(path.join(__dirname, './public', file));
+    response.send("Hello");
+    //var file = request.params[0] || 'views/index.html';
+    //
+    //response.sendFile(path.join(__dirname, './public', file));
 });
 
 app.listen(app.get('port'), function(){
